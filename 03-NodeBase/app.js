3bdff07d58d340f927e6b0bd7518f94086cd3b6d
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-03-25 14:05:00 -0400

    Node Base - Tabla del 5

---
+++ 03-NodeBase/app.js
@@ -0,0 +1,23 @@
+/**
+ *  working
+ */
+
+console.clear();
+console.log(`
+==========================
+        Tabla del 5
+==========================`);
+
+for (let n = 0; n <= 10; n++) {
+	r = n * 5;
+	console.log(`5 x ${n} = ${r}`);
+}
+
+/**
+ * Ejemplo de Fernando
+ *
+ * const base = 5;
+ * for(let i=0; i<=10; i++){
+ * console.log(`${base} x ${i} = ${base * i})}
+ *
+ */
