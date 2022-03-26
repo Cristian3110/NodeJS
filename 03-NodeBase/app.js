63e5827cfd1d02e65e7ee678e04dba26b73bff45
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-03-26 01:42:13 -0400

    Reuqerir paquetes -require

--- 03-NodeBase/app.js
+++ 03-NodeBase/app.js
@@ -2,22 +2,33 @@
  *  working
  */
 
+const fs = require('fs');
+
 console.clear();
 console.log(`
 ==========================
         Tabla del 5
 ==========================`);
 
-for (let n = 0; n <= 10; n++) {
-	r = n * 5;
-	console.log(`5 x ${n} = ${r}`);
-}
+//Mi código de la tarea
 
-/**
- * Ejemplo de Fernando
- *
- * const base = 5;
- * for(let i=0; i<=10; i++){
- * console.log(`${base} x ${i} = ${base * i})}
- *
- */
+// for (let n = 0; n <= 10; n++) {
+// 	r = n * 5;
+// 	console.log(`5 x ${n} = ${r}`);
+// }
+
+// Ejemplo de Fernando
+const base = 3;
+let salida = '';
+
+for (let i = 1; i <= 10; i++) {
+	salida += `${base} x ${i} = ${base * i} \n`;
+}
+console.log(salida);
+
+fs.writeFile(`table-${base}.txt`, salida, (err) => {
+	if (err) throw err;
+	console.log(`table-${base}.txt creado`);
+});
+
+console.log(salida);
