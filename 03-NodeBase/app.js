6f3cf1eba2c166eb64b44890ecf5d24efbe11638
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-03-31 01:36:22 -0400

    Aprendiendo sobre Yargs

--- 03-NodeBase/app.js
+++ 03-NodeBase/app.js
@@ -3,13 +3,19 @@
  *****************************************/
 
 //Importando la función del archivo multiplicar
-const { crearArchivo } = require('./helpers/multiplicar');
 
-const [, , arg3 = 'base=5'] = process.argv;
-const [, base = 5] = arg3.split('=');
+const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('yargs').argv;
+
+console.clear();
+
+console.log(process.argv);
+console.log(argv);
+// const [, , arg3 = 'base=5'] = process.argv;
+// const [, base = 5] = arg3.split('=');
 
 //
 
-crearArchivo(base)
-	.then((nombreArchivo) => console.log(nombreArchivo))
-	.catch((err) => console.log(err));
+// crearArchivo(base)
+// 	.then((nombreArchivo) => console.log(nombreArchivo))
+// 	.catch((err) => console.log(err));
