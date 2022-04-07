bf86cfcce255ce54de18a3bd4cc8887cfdc8650f
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-04-07 01:03:23 -0400

    Configurando la repetición del menú en forma infinita hasta la opción salir

--- 04-tareasHacer/helpers/mensajes.js
+++ 04-tareasHacer/helpers/mensajes.js
@@ -1,41 +1,48 @@
 require('colors');
 
 const mostrarMenu = () => {
-	console.clear();
-	console.log('======================='.red);
-	console.log('Seleccione una Opción'.yellow);
-	console.log('=======================\n'.red);
+	//Se crea promesa para resolver proble con la tarea procesada
+	return new Promise((resolve) => {
+		console.clear();
+		console.log('======================='.red);
+		console.log('Seleccione una Opción'.yellow);
+		console.log('=======================\n'.red);
 
-	console.log(`${'1.-'.yellow} Crear tarea`);
-	console.log(`${'2.-'.yellow} Listar tareas`);
-	console.log(`${'3.-'.yellow} Listar tares completadas`);
-	console.log(`${'4.-'.yellow} Listar tareas pendientes`);
-	console.log(`${'5.-'.yellow} Completar tarea(s)`);
-	console.log(`${'6.-'.yellow} Borrar tarea`);
-	console.log(`${'0.-'.yellow} Salir\n`);
+		console.log(`${'1.-'.yellow} Crear tarea`);
+		console.log(`${'2.-'.yellow} Listar tareas`);
+		console.log(`${'3.-'.yellow} Listar tares completadas`);
+		console.log(`${'4.-'.yellow} Listar tareas pendientes`);
+		console.log(`${'5.-'.yellow} Completar tarea(s)`);
+		console.log(`${'6.-'.yellow} Borrar tarea`);
+		console.log(`${'0.-'.yellow} Salir\n`);
 
-	// Configurando - Recibir una información del usuario
+		// Configurando - Recibir una información del usuario
 
-	const readline = require('readline').createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	});
+		const readline = require('readline').createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		});
 
-	readline.question('Seleccione una opción: ', (opt) => {
-		// console.log(opt);
-		readline.close();
+		readline.question('Seleccione una opción: ', (opt) => {
+			// console.log(opt);
+			readline.close();
+			resolve(opt);
+		});
 	});
 };
 
 const pausa = () => {
-	const readline = require('readline').createInterface({
-		input: process.stdin,
-		output: process.stdout,
-	});
+	return new Promise((resolve) => {
+		const readline = require('readline').createInterface({
+			input: process.stdin,
+			output: process.stdout,
+		});
 
-	readline.question(`Presione ${'\nENTER'.yellow} para continuar\n`, (opt) => {
-		// console.log(opt);
-		readline.close();
+		readline.question(`Presione ${'\nENTER'.yellow} para continuar\n`, (opt) => {
+			// console.log(opt);
+			readline.close();
+			resolve(opt);
+		});
 	});
 };
 
