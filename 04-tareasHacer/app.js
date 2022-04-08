853b8d3cd9e41c2214e89663fe9bc36df5295516
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-04-08 00:58:19 -0400

    lógica para el manejo de las tareas

--- 04-tareasHacer/app.js
+++ 04-tareasHacer/app.js
@@ -4,11 +4,12 @@
 require('colors');
 
 const { inquirerMenu, pausa } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
 
 // Esto lo utilizamos para hacerlo manualmente
 // const { mostrarMenu, pausa } = require('./helpers/mensajes');
 
-console.clear();
+// console.clear();
 const main = async () => {
 	console.log('Hola Mundo');
 
@@ -17,8 +18,13 @@ const main = async () => {
 	do {
 		opt = await inquirerMenu();
 		console.log({ opt });
+
+		// const tareas = new Tareas();
+
+		// tareas._listado[tarea.id] = tarea;
+		// console.log(tareas);
+		await pausa();
 		if (opt !== '0') {
-			await pausa();
 			// await pausa();
 		}
 	} while (opt !== '0');
---
+++ 04-tareasHacer/models/tareas.js
@@ -0,0 +1,16 @@
+/**
+ * Ejemplo de cómo queremos el listado
+ * _listado:
+ *      { 'uuid-35151-16516-3: {id:12, desc: kvbakvkjvb, completadoEn: 65165}}
+ *
+ */
+
+class Tareas {
+	_listado = {};
+
+	constructor() {
+		this._listado = {};
+	}
+}
+
+module.exports = Tareas;
