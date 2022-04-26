ba2c75ce73b9f66727b5488faaef83ce68b2ddd6
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-04-26 01:57:25 -0400

    Trabajando con Mapbox search API y token de acces

--- 05-climaApp/models/busquedas.js
+++ 05-climaApp/models/busquedas.js
@@ -19,7 +19,9 @@ class Busquedas {
 		// console.log('ciudad:', lugar);
 
 		try {
-			const resp = await axios.get('https://reqres.in/api/users?page=2');
+			const resp = await axios.get(
+				'https://api.mapbox.com/geocoding/v5/mapbox.places/Caracas%2C%20Distri%2C%20Venezuela.json?limit=5&language=es&access_token=pk.eyJ1IjoibWFrb3RvMzExMCIsImEiOiJjbDJmbzJrem0wMHcxM2lzMmlnMHltNDMyIn0.amKCl6IarW_E72BXk2Y3EA'
+			);
 			console.log(resp.data);
 
 			return []; // retorna el lugar de busqueda
