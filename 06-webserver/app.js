a0acce2514e8fddf592abb8287be14eabb43b81f
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-05-08 14:45:08 -0400

    Request y Response

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -6,7 +6,24 @@ const http = require('http');
 
 http
 	.createServer((req, res) => {
-		res.write('Hola mundo');
+		/**
+		 * Nota: En el servidor podemos trabajar con distintos tipos de archivo, ya sean csv, de texto, entre
+		 * otros.
+		 * Se puede trabajar de distintas maneras el request y el response, que son el requerimiento que hace
+		 * el cliente, y la respi¿uesta que nosotros configuramos desde el servicio.
+		 */
+
+		// trabajando con archivos CSV
+		res.setHeader('Content-Disposition', 'attachment; filename=lista.csv');
+		res.writeHead(200, { 'Content-Type': 'application/csv' });
+		// res.write('Hola mundo');
+
+		res.write('id, nombre\n');
+		res.write('id, Cristian\n');
+		res.write('id, Carmen\n');
+		res.write('id, Nemecio\n');
+		res.write('id, Maricarmen\n');
+		res.write('id, Angel\n');
 		res.end();
 	})
 	.listen(8080);
