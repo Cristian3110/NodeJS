64de5015d6ff3748524ba7f33cd619551b04d04b
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-05-09 10:13:14 -0400

    Servir contenido estático

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -7,20 +7,29 @@ const app = express();
 
 const port = 8080;
 
+//Servir contenido estático
+// middleware
+// Ahora el siguiente path será la ruta por defecto
+app.use(express.static('public'));
+
 // Rutas de nuestro servicio
 
-app.get('/', (req, res) => {
-	res.send('Home Page');
-});
+//* Se comenta la siguiente línea ya que con el middleware apuntamos a la ruta de la carpeta public
+// app.get('/', (req, res) => {
+// 	res.send('Home Page');
+// });
 
 app.get('/hola-mundo', (req, res) => {
 	res.send('Hola mundo en su ruta respectiva');
 });
 
-// Cualquier otra ruta que no sea especificada indicar lo siguiente
+//* Si queremos mandar una ruta de algún archivo
 app.get('*', (req, res) => {
-	res.send('404 | Page not found');
+	res.sendFile(__dirname + '/public/404.html');
 });
+
+// Cualquier otra ruta que no sea especificada indicar lo siguiente
+// s
 
 app.listen(port, () => {
 	console.log(`App Escuchando el puerto: ${port}`);
