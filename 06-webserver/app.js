50e47266b77883481ce034bcd39a16ec888713a7
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-05-09 19:18:25 -0400

    servir un sitio web completo

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -19,17 +19,23 @@ app.use(express.static('public'));
 // 	res.send('Home Page');
 // });
 
+app.get('/generic', (req, res) => {
+	res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+	res.sendFile(__dirname + '/public/elements.html');
+});
+
 app.get('/hola-mundo', (req, res) => {
 	res.send('Hola mundo en su ruta respectiva');
 });
 
+// Cualquier otra ruta que no sea especificada indicar lo siguiente
 //* Si queremos mandar una ruta de algún archivo
 app.get('*', (req, res) => {
-	res.sendFile(__dirname + '/public/404.html');
+	res.sendFile(__dirname + '/public/before-page/404.html');
 });
-
-// Cualquier otra ruta que no sea especificada indicar lo siguiente
-// s
 
 app.listen(port, () => {
 	console.log(`App Escuchando el puerto: ${port}`);
