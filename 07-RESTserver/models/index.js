5cad3b35fca97d08bc21f40692fae5cac2b67119
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-07-05 12:31:17 -0400

    optimizando las importaciones de los modelos

---
+++ 07-RESTserver/models/index.js
@@ -0,0 +1,13 @@
+//Optimizando las importanciones de los modelos
+
+const Categoria = require('./categorias');
+const Server = require('./server');
+const Role = require('./role');
+const Usuario = require('./usuario');
+
+module.exports = {
+	Categoria,
+	Server,
+	Role,
+	Usuario,
+};
