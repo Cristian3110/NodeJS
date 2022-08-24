1bc11c24aad1161e14489d05fe434d6603cd0a48
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-08-24 00:39:13 -0400

    mensajes de conección y desconección con boostrap en html

--- 08-webSocket-server/public/js/socket-client.js
+++ 08-webSocket-server/public/js/socket-client.js
@@ -1,17 +1,24 @@
 /**
  * Archivo en comunicación con el server de websocket
  */
-
 console.log('Hello world with websockets');
+
+// Referencia del HTML
+const msgOnline = document.querySelector('#msgOnline');
+const msgOffline = document.querySelector('#msgOffline');
 
 // configuración de conección del frontEnd
 const socket = io();
 
-// para escuchar eventos
+// para escuchar eventos desde front
 socket.on('connect', () => {
 	console.log('connect');
+	msgOffline.style.display = 'none';
+	msgOnline.style.display = '';
 });
 
 socket.on('disconnect', () => {
-	console.log('Server Disconnect  ');
+	console.log('Server Disconnect');
+	msgOnline.style.display = 'none';
+	msgOffline.style.display = '';
 });
