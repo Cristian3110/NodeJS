6798e9f135059aaa9fdd78c32b36ea23a899b184
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-08-24 00:13:14 -0400

    configurando mensaje de conectado y desconectado desde el frontEnd

--- 08-webSocket-server/public/js/socket-client.js
+++ 08-webSocket-server/public/js/socket-client.js
@@ -6,3 +6,12 @@ console.log('Hello world with websockets');
 
 // configuración de conección del frontEnd
 const socket = io();
+
+// para escuchar eventos
+socket.on('connect', () => {
+	console.log('connect');
+});
+
+socket.on('disconnect', () => {
+	console.log('Server Disconnect  ');
+});
