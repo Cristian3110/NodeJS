c66c9716a63e14c2a1234513a7573c81820f836b
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-08-29 13:49:27 -0400

    Configurando pantalla de Escritorio

--- 09-SocketsColas/public/js/escritorio.js
+++ 09-SocketsColas/public/js/escritorio.js
@@ -1,3 +1,41 @@
+/**
+ * Configuración de pantalla de escritorio
+ */
 
+//Referencias HTML
 
-console.log('Escritorio HTML');
+const lblEscritorio = document.querySelector('h1');
+const btnAtender = document.querySelector('button');
+
+const searchParams = new URLSearchParams(window.location.search);
+
+if (!searchParams.has('escritorio')) {
+	window.location = 'index.html';
+	throw new Error('El escritorio es obligatorio');
+}
+
+const escritorio = searchParams.get('escritorio');
+lblEscritorio.innerText = escritorio;
+
+console.log({ escritorio });
+
+const socket = io();
+
+socket.on('connect', () => {
+	btnAtender.disabled = false;
+});
+
+socket.on('ultimo-ticket', (ultimo) => {
+	// lblNuevoTicket.innerText = `Ticket:${ultimo}`;
+});
+
+socket.on('disconnect', () => {
+	btnAtender.disabled = true;
+});
+
+btnAtender.addEventListener('click', () => {
+	// socket.emit('siguiente-ticket', null, (ticket) => {
+	// 	console.log('Desde el server', ticket);
+	// 	lblNuevoTicket.innerText = ticket;
+	// });
+});
