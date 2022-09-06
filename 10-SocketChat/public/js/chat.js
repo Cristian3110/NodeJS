bc68ce1bdddd8942d11420460d091b08e013dd18
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-09-06 12:13:41 -0400

    validando token - frontend, configurado

--- 10-SocketChat/public/js/chat.js
+++ 10-SocketChat/public/js/chat.js
@@ -1,5 +1,35 @@
-/***
- * configurando nuestro socket client
- */
+// ternary operator
+const url = window.location.hostname.includes('localhost')
+	? 'http://localhost:8080/api/auth/'
+	: 'https://restserver-production.up.railway.app/api/auth/';
 
-const socket = io();
+let usuario = null;
+let socket = null;
+
+const validarJWT = async () => {
+	const token = localStorage.getItem('token') || '';
+
+	if (token.length <= 10) {
+		window.location = 'index.html';
+		throw new Error('No hay token en nuestro servidor');
+	}
+
+	const resp = await fetch(url, {
+		headers: { 'x-token': token },
+	});
+
+	const { usuario: userDB, token: tokenDB } = await resp.json();
+
+	localStorage.setItem('token', tokenDB);
+	usuario = userDB;
+	console.log(userDB, tokenDB);
+};
+
+const main = async () => {
+	await validarJWT();
+};
+
+main();
+//configurando nuestro socket client
+
+// const socket = io();
